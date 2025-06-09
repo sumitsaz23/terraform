31e893dfbe5d1f652b72c35a93021a8171cccefd
sumits <sumit.sur@outlook.com>
2025-06-09 07:43:45 +0530

    d

---
+++ python-web-app/main.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.aws_region
+}
+
+
+# Create a VPC
+resource "aws_vpc" "vpc_python_web_app" {
+  cidr_block       = var.vpc_cidr_block_python_web_app
+  instance_tenancy = "default"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = var.VPC_name
+    CreatedBy = "Terraform"
+    app = "python_web_app"
+
+  }
+}
+
+# Create Subnets
+
+resource "aws_subnet" "py_web_app_subnet_1a_Public" {
+  vpc_id            = aws_vpc.var.vpc_name.id
+  cidr_block        = var.py_web_app_subnet_1a_Public_cidr_block
+  availability_zone = "ap-south-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "var.py_web_app_subnet_1a_Public_name"
+    CreatedBy = "Terraform"
+    app = "python_web_app"
+ }
+}
+
+
+resource "aws_subnet" "py_web_app_subnet_1b_Public" {
+  vpc_id            = aws_vpc.var.vpc_name.id
+  cidr_block        = var.py_web_app_subnet_1b_Public_cidr_block
+  availability_zone = "ap-south-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "var.py_web_app_subnet_1b_Public_name"
+    CreatedBy = "Terraform"
+    app = "python_web_app"
+ }
+  
+}
---
+++ python-web-app/variables.tf
@@ -0,0 +1,83 @@
+variable "aws_region" {
+  description = "The AWS region to deploy the resources"
+  type        = string
+  default     = "ap-south-1"
+  
+}
+
+
+
+variable "app_name" {
+  description = "The name of the web application"
+  type        = string
+  default     = "python_web_app" 
+}
+
+variable "vpc_cidr_block_python_web_app" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0/16"
+  
+}
+
+variable "VPC_name" {
+  description = "The name of the VPC"
+  type        = string
+  default     = "py_web_app_vpc"
+  
+}
+
+variable "py_web_app_subnet_1a_Public_name" {
+  description = "The name of the first public subnet in availability zone 1a"
+  type        = string
+  default     = "py_web_app_subnet_1a_Public"
+  
+}
+variable "py_web_app_subnet_1a_Public_cidr_block" {
+  description = "The CIDR block for the first subnet in availability zone 1a"
+  type        = string
+  default     = "10.0.0.0/25"
+  
+}
+
+variable "py_web_app_subnet_1b_Public_name" {
+  description = "The name of the first public subnet in availability zone 1b"
+  type        = string
+  default     = "py_web_app_subnet_1b_Public"
+
+}
+
+variable "py_web_app_subnet_1b_Public_cidr_block" {
+  description = "The CIDR block for the first subnet in availability zone 1b"
+  type        = string
+  default     = "10.0.0.128/25"
+  
+}
+
+variable "py_web_app_subnet_1a_private_name" {
+  description = "The name of the first private subnet in availability zone 1a"
+  type        = string
+  default     = "py_web_app_subnet_1a_Private"
+  
+}
+
+variable "py_web_app_subnet_1a_private_cidr_block" {
+  description = "The CIDR block for the first subnet in availability zone 1a"
+  type        = string
+  default     = "10.1.0.0/24"
+  
+}
+
+variable "py_web_app_subnet_1b_private_name" {
+  description = "The name of the first private subnet in availability zone 1b"
+  type        = string
+  default     = "py_web_app_subnet_1b_Private"
+  
+}
+
+variable "py_web_app_subnet_1b_private_cidr_block" {
+  description = "The CIDR block for the first subnet in availability zone 1b"
+  type        = string
+  default     = "10.2.0.0/24"
+  
+}
